9e26636058d6601b5489f417539b5fe7176c10e0
Abduroxman-Qahramonov-04 <abdurohman.kahramonov@servpoint.org>
2024-08-05 18:15:37 +0500

    After annotations, loading method calls after execution

--- src/main/java/com/example/aopPractice/aspects/LoggingAspect.java
+++ src/main/java/com/example/aopPractice/aspects/LoggingAspect.java
@@ -1,12 +1,12 @@
 package com.example.aopPractice.aspects;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Objects;
 
 //1 Configuration
 //2 AOP
@@ -21,5 +21,19 @@ public class LoggingAspect {
     public void logMethodCall(JoinPoint joinPoint){
         //Logging - what?
         logger.info("Before Aspect - {} is called - {}", joinPoint,joinPoint.getArgs());
+    }
+    @AfterReturning(
+            pointcut = "execution(* com.example.aopPractice.*.*.*(..)) ",
+            returning = "resultValue"
+    )
+    public void loadMethodCallAfterExecution(JoinPoint joinPoint, Object resultValue){
+        logger.info("After Returning Aspect - {} has returned {} ",joinPoint,resultValue);
+    }
+    @AfterThrowing(
+            pointcut = "execution(* com.example.aopPractice.*.*.*(..)) ",
+            throwing = "exception"
+    )
+    public void loadMethodCallAfterExecution(JoinPoint joinPoint, Exception exception){
+        logger.info("After Returning Aspect - {} has thrown {} ",joinPoint,exception);
     }
 }
